62877c6efa935b1fc0e31433a71f061cbefe30c6
Charlie Vieth <cvieth@lyft.com>
2018-09-27 14:24:59 -0400

    gowd: remove binary and simplify

--- gowd/gowd.go
+++ gowd/gowd.go
@@ -4,37 +4,25 @@ import (
 	"fmt"
 	"go/build"
 	"os"
-	"path/filepath"
-	"runtime"
 )
 
-func main() {
+func realMain() (string, error) {
 	wd, err := os.Getwd()
 	if err != nil {
-		Fatal(err)
+		return "", err
 	}
 	pkg, err := build.ImportDir(wd, build.FindOnly)
 	if err != nil {
-		Fatal(err)
+		return "", err
 	}
-	fmt.Println(pkg.ImportPath)
+	return pkg.ImportPath, nil
 }
 
-func Fatal(err interface{}) {
-	if err == nil {
-		return
+func main() {
+	gowd, err := realMain()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
 	}
-	var format string
-	if _, file, line, ok := runtime.Caller(1); ok && file != "" {
-		format = fmt.Sprintf("Error (%s:%d)", filepath.Base(file), line)
-	} else {
-		format = "Error"
-	}
-	switch err.(type) {
-	case error, string:
-		fmt.Fprintf(os.Stderr, "%s: %s\n", format, err)
-	default:
-		fmt.Fprintf(os.Stderr, "%s: %#v\n", format, err)
-	}
-	os.Exit(1)
+	fmt.Println(gowd)
 }
