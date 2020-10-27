7cf3aa0a5961e6697a09f94d1b255e2873728fdd
Charlie Vieth <cvieth@lyft.com>
2020-10-27 13:10:41 -0400

    sortimports: fix

--- sortimports/main.go
+++ sortimports/main.go
@@ -2,420 +2,208 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"go/ast"
+	"go/format"
 	"go/parser"
-	"go/printer"
+	"go/scanner"
 	"go/token"
+	"io/ioutil"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"runtime"
-	"sort"
 	"strconv"
+	"strings"
+
+	"golang.org/x/tools/go/ast/astutil"
+	"golang.org/x/tools/imports"
 )
 
-/*func SortImports(filename string) error {
+var (
+	zeroDelim  = false
+	formatOnly = false
+
+	options = &imports.Options{
+		Fragment:  true,
+		AllErrors: true,
+		Comments:  true,
+		TabIndent: true,
+		TabWidth:  8,
+	}
+	exitCode = 0
+)
+
+func init() {
+	flag.StringVar(&imports.LocalPrefix, "local", "",
+		"put imports beginning with this string after 3rd-party packages; "+
+			"comma-separated list")
+
+	flag.BoolVar(&formatOnly, "sort-only", false,
+		"if true, don't fix imports and only format and sort imports")
+
+	flag.BoolVar(&zeroDelim, "0", false,
+		"expect NUL ('\\0') characters as separators, instead of newlines "+
+			"when reading files from STDIN")
+}
+
+func report(err error) {
+	scanner.PrintError(os.Stderr, err)
+	exitCode = 2
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s [flags] [path ...]\n", filepath.Base(os.Args[0]))
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func isGoFile(f os.FileInfo) bool {
+	// ignore non-Go files
+	name := f.Name()
+	return !f.IsDir() && !strings.HasPrefix(name, ".") && strings.HasSuffix(name, ".go")
+}
+
+func unquote(s string) string {
+	if len(s) != 0 && s[0] == '`' || s[0] == '"' {
+		return s[1 : len(s)-1]
+	}
+	return s
+}
+
+func processFile(filename string) error {
 	fset := token.NewFileSet()
-	af, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
+
+	fi, err := os.Lstat(filename)
 	if err != nil {
 		return err
 	}
-	ast.SortImports(fset, af)
-
-	var out bytes.Buffer
-	if err := printer.Fprint(&out, fset, af); err != nil {
-		return err
-	}
-
-	f, err := os.OpenFile(filename, os.O_TRUNC|os.O_WRONLY, 0)
+	src, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return err
 	}
-	defer f.Close()
-	if _, err := out.WriteTo(f); err != nil {
+
+	af, err := parser.ParseFile(fset, filename, src, parser.ParseComments)
+	if err != nil {
 		return err
 	}
-	return nil
-}
-*/
 
-func importPath(s ast.Spec) string {
-	t, err := strconv.Unquote(s.(*ast.ImportSpec).Path.Value)
-	if err == nil {
-		return t
-	}
-	return ""
-}
-
-func importName(s ast.Spec) string {
-	n := s.(*ast.ImportSpec).Name
-	if n == nil {
-		return ""
-	}
-	return n.Name
-}
-
-func importComment(s ast.Spec) string {
-	c := s.(*ast.ImportSpec).Comment
-	if c == nil {
-		return ""
-	}
-	return c.Text()
-}
-
-func SortImports(fset *token.FileSet, f *ast.File) {
-	// CEV: I'm too lazy to figure out all the logic for merging empty lines
-	// but running it twice works - so we run it three times just to be sure.
-	for i := 0; i < 5; i++ {
-		for _, d := range f.Decls {
-			d, ok := d.(*ast.GenDecl)
-			if !ok || d.Tok != token.IMPORT {
-				// Not an import declaration, so we're done.
-				// Imports are always first.
-				break
-			}
-
-			if !d.Lparen.IsValid() {
-				// Not a block: sorted by default.
-				continue
-			}
-
-			// Identify and sort runs of specs on successive lines.
-			i := 0
-			specs := d.Specs[:0]
-			for j, s := range d.Specs {
-				if j > i && fset.Position(s.Pos()).Line > 1+fset.Position(d.Specs[j-1].End()).Line {
-					// j begins a new run. End this one.
-					specs = append(specs, d.Specs[i:j]...)
-					fset.File(d.Specs[j-1].End()).MergeLine(fset.Position(d.Specs[j-1].End()).Line)
-					i = j
-				}
-			}
-			specs = append(specs, d.Specs[i:]...)
-
-			// TODO: try to change the position of the specs
-			//
-			// fset.File(1).SetLines(lines)
-
-			sort.Slice(specs, func(i, j int) bool {
-				ipath := importPath(specs[i])
-				jpath := importPath(specs[j])
-				if ipath != jpath {
-					return ipath < jpath
-				}
-				iname := importName(specs[i])
-				jname := importName(specs[j])
-				if iname != jname {
-					return iname < jname
-				}
-				return importComment(specs[i]) < importComment(specs[j])
-			})
-			d.Specs = specs
-
-			// Deduping can leave a blank line before the rparen; clean that up.
-			if len(d.Specs) > 0 {
-				lastSpec := d.Specs[len(d.Specs)-1]
-				lastLine := fset.Position(lastSpec.Pos()).Line
-				rParenLine := fset.Position(d.Rparen).Line
-				for rParenLine > lastLine+1 {
-					rParenLine--
-					fset.File(d.Rparen).MergeLine(rParenLine)
-				}
+	imps := astutil.Imports(fset, af)
+	for _, block := range imps {
+		if len(block) == 1 && unquote(block[0].Path.Value) == "C" {
+			continue
+		}
+		for _, m := range block {
+			if m.Name != nil {
+				astutil.DeleteNamedImport(fset, af, m.Name.Name, unquote(m.Path.Value))
+			} else {
+				astutil.DeleteImport(fset, af, unquote(m.Path.Value))
 			}
 		}
 	}
+
+	for _, block := range imps {
+		if len(block) == 1 && block[0].Path.Value == `"C"` {
+			continue
+		}
+		for _, m := range block {
+			if m.Name != nil {
+				path, _ := strconv.Unquote(m.Path.Value)
+				astutil.AddNamedImport(fset, af, m.Name.Name, path)
+			} else {
+				path, _ := strconv.Unquote(m.Path.Value)
+				astutil.AddImport(fset, af, path)
+			}
+		}
+	}
+
+	var buf bytes.Buffer
+	if err := format.Node(&buf, fset, af); err != nil {
+		return err
+	}
+	if !bytes.Equal(buf.Bytes(), src) {
+		if err := ioutil.WriteFile(filename, buf.Bytes(), fi.Mode()); err != nil {
+			return err
+		}
+	}
+
+	res, err := imports.Process(filename, buf.Bytes(), options)
+	if err != nil {
+		return err
+	}
+	if bytes.Equal(src, res) {
+		return nil
+	}
+
+	return ioutil.WriteFile(filename, res, fi.Mode())
 }
 
-func FixFile(name string) error {
-	fset := token.NewFileSet()
-
-	af, err := parser.ParseFile(fset, name, nil, parser.ParseComments)
+func visitFile(path string, f os.FileInfo, err error) error {
+	if err == nil && isGoFile(f) {
+		err = processFile(path)
+	}
 	if err != nil {
-		Fatal(err)
+		report(err)
 	}
-	SortImports(fset, af)
+	return nil
+}
 
-	var out bytes.Buffer
-	if err := printer.Fprint(&out, fset, af); err != nil {
-		return err
-	}
+func walkDir(path string) {
+	filepath.Walk(path, visitFile)
+}
 
-	fi, err := os.Lstat(name)
+func readFilesFromStdin() ([]string, error) {
+	in, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	tmpname := name + ".tmp"
-	f, err := os.OpenFile(tmpname, os.O_CREATE|os.O_EXCL|os.O_WRONLY, fi.Mode())
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	if _, err := out.WriteTo(f); err != nil {
-		return err
-	}
-	if err := f.Close(); err != nil {
-		return err
+	var delim byte
+	if !zeroDelim {
+		delim = '\n'
 	}
 
-	return os.Rename(tmpname, name)
+	a := bytes.Split(in, []byte{delim})
+
+	paths := make([]string, 0, len(a))
+	for _, b := range a {
+		s := string(b)
+		if strings.TrimSpace(s) == "" {
+			continue
+		}
+		paths = append(paths, s)
+	}
+	return paths, nil
+}
+
+func realMain() {
+	flag.Usage = usage
+	flag.Parse()
+
+	paths := flag.Args()
+
+	// read files from stdin
+	if len(paths) == 0 || paths[0] == "-" {
+		var err error
+		paths, err = readFilesFromStdin()
+		if err != nil {
+			report(err)
+		}
+		return
+	}
+
+	for _, path := range flag.Args() {
+		switch dir, err := os.Stat(path); {
+		case err != nil:
+			report(err)
+		case dir.IsDir():
+			walkDir(path)
+		default:
+			if err := processFile(path); err != nil {
+				report(err)
+			}
+		}
+	}
 }
 
 func main() {
-	localPkg := flag.String("local", "", "put imports beginning with this string after 3rd-party packages; comma-separated list")
-	flag.Parse()
-	_ = localPkg
-
-	if flag.NArg() == 0 {
-		return
-	}
-
-	var failed bool
-	for _, filename := range flag.Args() {
-		if err := FixFile(filename); err != nil {
-			fmt.Fprintf(os.Stderr, "error: %s: %s\n", filename, err)
-			failed = true
-		}
-	}
-	if failed {
-		os.Exit(1)
-	}
-
-	args := []string{"-w"}
-	if *localPkg != "" {
-		args = append(args, "-local", *localPkg)
-	}
-	args = append(args, flag.Args()...)
-
-	cmd := exec.Command("goimports", args...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		Fatal(err)
-	}
+	realMain()
+	os.Exit(exitCode)
 }
-
-func PrintJSON(v interface{}) error {
-	enc := json.NewEncoder(os.Stdout)
-	enc.SetIndent("", "    ")
-	return enc.Encode(v)
-}
-
-func Fatal(err interface{}) {
-	if err == nil {
-		return
-	}
-	var s string
-
-	if _, file, line, ok := runtime.Caller(1); ok && file != "" {
-		s = fmt.Sprintf("Error (%s:%d)", filepath.Base(file), line)
-	} else {
-		s = "Error"
-	}
-	switch err.(type) {
-	case error, string, fmt.Stringer:
-		fmt.Fprintf(os.Stderr, "%s: %s\n", s, err)
-	default:
-		fmt.Fprintf(os.Stderr, "%s: %#v\n", s, err)
-	}
-	os.Exit(1)
-}
-
-/*
-
-// collapse indicates whether prev may be removed, leaving only next.
-func collapse(prev, next ast.Spec) bool {
-	if importPath(next) != importPath(prev) || importName(next) != importName(prev) {
-		return false
-	}
-	return prev.(*ast.ImportSpec).Comment == nil
-}
-
-type posSpan struct {
-	Start token.Pos
-	End   token.Pos
-}
-
-func sortSpecs(fset *token.FileSet, f *ast.File, specs []ast.Spec) []ast.Spec {
-	// Can't short-circuit here even if specs are already sorted,
-	// since they might yet need deduplication.
-	// A lone import, however, may be safely ignored.
-	if len(specs) <= 1 {
-		return specs
-	}
-
-	// Record positions for specs.
-	pos := make([]posSpan, len(specs))
-	for i, s := range specs {
-		pos[i] = posSpan{s.Pos(), s.End()}
-	}
-
-	// Identify comments in this range.
-	// Any comment from pos[0].Start to the final line counts.
-	lastLine := fset.Position(pos[len(pos)-1].End).Line
-	cstart := len(f.Comments)
-	cend := len(f.Comments)
-	for i, g := range f.Comments {
-		if g.Pos() < pos[0].Start {
-			continue
-		}
-		if i < cstart {
-			cstart = i
-		}
-		if fset.Position(g.End()).Line > lastLine {
-			cend = i
-			break
-		}
-	}
-	comments := f.Comments[cstart:cend]
-
-	// Assign each comment to the import spec preceding it.
-	importComment := map[*ast.ImportSpec][]*ast.CommentGroup{}
-	specIndex := 0
-	for _, g := range comments {
-		for specIndex+1 < len(specs) && pos[specIndex+1].Start <= g.Pos() {
-			specIndex++
-		}
-		s := specs[specIndex].(*ast.ImportSpec)
-		importComment[s] = append(importComment[s], g)
-	}
-
-	// Sort the import specs by import path.
-	// Remove duplicates, when possible without data loss.
-	// Reassign the import paths to have the same position sequence.
-	// Reassign each comment to abut the end of its spec.
-	// Sort the comments by new position.
-	sort.Sort(byImportSpec(specs))
-
-	// Dedup. Thanks to our sorting, we can just consider
-	// adjacent pairs of imports.
-	deduped := specs[:0]
-	for i, s := range specs {
-		if i == len(specs)-1 || !collapse(s, specs[i+1]) {
-			deduped = append(deduped, s)
-		} else {
-			p := s.Pos()
-			fset.File(p).MergeLine(fset.Position(p).Line)
-		}
-	}
-	specs = deduped
-
-	// Fix up comment positions
-	for i, s := range specs {
-		s := s.(*ast.ImportSpec)
-		if s.Name != nil {
-			s.Name.NamePos = pos[i].Start
-		}
-		s.Path.ValuePos = pos[i].Start
-		s.EndPos = pos[i].End
-		nextSpecPos := pos[i].End
-
-		for _, g := range importComment[s] {
-			for _, c := range g.List {
-				c.Slash = pos[i].End
-				nextSpecPos = c.End()
-			}
-		}
-		if i < len(specs)-1 {
-			pos[i+1].Start = nextSpecPos
-			pos[i+1].End = nextSpecPos
-		}
-	}
-
-	sort.Sort(byCommentPos(comments))
-
-	// Fixup comments can insert blank lines, because import specs are on different lines.
-	// We remove those blank lines here by merging import spec to the first import spec line.
-	firstSpecLine := fset.Position(specs[0].Pos()).Line
-	for _, s := range specs[1:] {
-		p := s.Pos()
-		line := fset.File(p).Line(p)
-		for previousLine := line - 1; previousLine >= firstSpecLine; {
-			fset.File(p).MergeLine(previousLine)
-			previousLine--
-		}
-	}
-	return specs
-}
-
-type byImportSpec []ast.Spec // slice of *ast.ImportSpec
-
-func (x byImportSpec) Len() int      { return len(x) }
-func (x byImportSpec) Swap(i, j int) { x[i], x[j] = x[j], x[i] }
-func (x byImportSpec) Less(i, j int) bool {
-	ipath := importPath(x[i])
-	jpath := importPath(x[j])
-
-	igroup := importGroup(ipath)
-	jgroup := importGroup(jpath)
-	if igroup != jgroup {
-		return igroup < jgroup
-	}
-
-	if ipath != jpath {
-		return ipath < jpath
-	}
-	iname := importName(x[i])
-	jname := importName(x[j])
-
-	if iname != jname {
-		return iname < jname
-	}
-	return importComment(x[i]) < importComment(x[j])
-}
-
-type byCommentPos []*ast.CommentGroup
-
-func (x byCommentPos) Len() int           { return len(x) }
-func (x byCommentPos) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
-func (x byCommentPos) Less(i, j int) bool { return x[i].Pos() < x[j].Pos() }
-
-// LocalPrefix is a comma-separated string of import path prefixes, which, if
-// set, instructs Process to sort the import paths with the given prefixes
-// into another group after 3rd-party packages.
-const LocalPrefix = ""
-
-func localPrefixes() []string {
-	if LocalPrefix != "" {
-		return strings.Split(LocalPrefix, ",")
-	}
-	return nil
-}
-
-// importToGroup is a list of functions which map from an import path to
-// a group number.
-var importToGroup = []func(importPath string) (num int, ok bool){
-	func(importPath string) (num int, ok bool) {
-		for _, p := range localPrefixes() {
-			if strings.HasPrefix(importPath, p) || strings.TrimSuffix(p, "/") == importPath {
-				return 3, true
-			}
-		}
-		return
-	},
-	func(importPath string) (num int, ok bool) {
-		if strings.HasPrefix(importPath, "appengine") {
-			return 2, true
-		}
-		return
-	},
-	func(importPath string) (num int, ok bool) {
-		if strings.Contains(importPath, ".") {
-			return 1, true
-		}
-		return
-	},
-}
-
-func importGroup(importPath string) int {
-	for _, fn := range importToGroup {
-		if n, ok := fn(importPath); ok {
-			return n
-		}
-	}
-	return 0
-}
-*/
