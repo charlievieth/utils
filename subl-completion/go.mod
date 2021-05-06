05cf80b0c4bf5b889afa9aac3813ff0a0a120a20
Charlie Vieth <charlie@cockroachlabs.com>
2021-05-06 13:58:49 -0400

    sub-completion: use github.com/posener/complete/v2 for completion

---
+++ subl-completion/go.mod
@@ -0,0 +1,5 @@
+module github.com/charlievieth/utils/subl-completion
+
+go 1.16
+
+require github.com/posener/complete/v2 v2.0.1-alpha.13
--- subl-completion/main.go
+++ subl-completion/main.go
@@ -1,43 +1,32 @@
-// subl-completion is a package for generating auto complete
-// commands for sublime text.
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/spf13/cobra"
+	"github.com/posener/complete/v2"
+	"github.com/posener/complete/v2/predict"
 )
 
-func SublCommand() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:        "subl",
-		Aliases:    []string{"subl"},
-		SuggestFor: []string{"subl"},
-	}
-	flags := cmd.PersistentFlags()
-	flags.SortFlags = false
-
-	flags.String("project", "", "Load the given project")
-	cmd.MarkPersistentFlagFilename("project", "sublime-project")
-
-	flags.String("command", "", "Run the given command")
-
-	flags.BoolP("new-window", "n", false, "Open a new window")
-	flags.BoolP("add", "a", false, "Add folders to the current window")
-	flags.BoolP("wait", "w", false, "Wait for the files to be closed before returning")
-	flags.BoolP("background", "b", false, "Don't activate the application")
-	flags.BoolP("stay", "s", false, "Keep the application activated after closing the file")
-	flags.BoolP("help", "h", false, "Show help (this message) and exit")
-	flags.BoolP("version", "v", false, "Show version and exit")
-
-	return cmd
-}
-
 func main() {
-	err := SublCommand().GenBashCompletion(os.Stdout)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error:", err)
-		os.Exit(1)
+	subl := &complete.Command{
+		Flags: map[string]complete.Predictor{
+			"a ":                   predict.Nothing,
+			"add":                  predict.Nothing,
+			"w ":                   predict.Nothing,
+			"wait":                 predict.Nothing,
+			"b ":                   predict.Nothing,
+			"background":           predict.Nothing,
+			"s ":                   predict.Nothing,
+			"stay":                 predict.Nothing,
+			"safe-mode":            predict.Nothing,
+			"h ":                   predict.Nothing,
+			"help":                 predict.Nothing,
+			"v ":                   predict.Nothing,
+			"version":              predict.Nothing,
+			"n ":                   predict.Nothing,
+			"new-window":           predict.Nothing,
+			"command":              predict.Nothing,
+			"launch-or-new-window": predict.Nothing,
+			"project":              predict.Files("*.sublime-project"),
+		},
 	}
+	subl.Complete("subl")
 }
