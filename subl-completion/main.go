2751c4a745f83653f2c6b0d67974c19bf1b72d31
Charlie Vieth <charlie@cockroachlabs.com>
2021-06-02 11:53:06 -0400

    subl-completion: improve file and dir completion
    Improve file and dir completion and ignore binary files.

--- subl-completion/main.go
+++ subl-completion/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"bytes"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 	"sort"
-	"time"
+	"strings"
 
 	"github.com/posener/complete/v2"
 	"github.com/posener/complete/v2/predict"
@@ -23,36 +26,159 @@ func isBinary(buf []byte, name string) bool {
 	return bytes.IndexByte(buf[:n], 0) != -1
 }
 
-func Complete(prefix string) (options []string) {
-	dirs := make(chan []string, 1)
-	go func() { dirs <- predict.Dirs("*").Predict(prefix) }()
+func isExecutable(file string, mode os.FileMode) bool {
+	if runtime.GOOS != "windows" {
+		return !mode.IsDir() && mode&0111 != 0
+	}
+	_, err := exec.LookPath(file)
+	return err == nil
+}
 
-	buf := make([]byte, 512)
-	names := predict.Files("*").Predict(prefix)
-	a := names[:0]
-	for _, name := range names {
-		if !isBinary(buf, name) {
-			a = append(a, name)
+// TODO: don't exclude supported image formats
+func isImage(name string) bool {
+	switch strings.ToLower(filepath.Ext(name)) {
+	case "bmp", "gif", "jpeg", "jpg", "pdf", "png", "raw", "svg", "webp":
+		return true
+	}
+	return false
+}
+
+func readdirnames(name string) ([]string, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	names, err := f.Readdirnames(-1)
+	f.Close()
+	return names, err
+}
+
+func CompleteProject(prefix string) []string {
+	dir, base := filepath.Split(prefix)
+	openDir := dir
+	if dir == "" {
+		openDir = "."
+	}
+	names, err := readdirnames(openDir)
+	if err != nil {
+		return nil
+	}
+	var matches []string
+	for _, s := range names {
+		if strings.HasSuffix(s, ".sublime-project") && HasPrefix(s, base) {
+			matches = append(matches, filepath.Join(dir, s))
 		}
 	}
-	names = a
-
-	select {
-	case d := <-dirs:
-		names = append(names, d...)
-		sort.Strings(names)
-	case <-time.After(time.Millisecond * 200):
-		// ok
+	if len(matches) != 0 {
+		sort.Strings(matches)
 	}
-	return names
+	return matches
+}
+
+func HasPrefix(s, prefix string) bool {
+	if runtime.GOOS == "darwin" || runtime.GOOS == "windows" {
+		return len(s) >= len(prefix) && (s[0:len(prefix)] == prefix ||
+			strings.EqualFold(s[0:len(prefix)], prefix))
+	}
+	return len(s) >= len(prefix) && s[0:len(prefix)] == prefix
+}
+
+// Only check for binary files when there are 64 or fewer possible matches
+const BinaryFileCutoff = 64
+
+func CompleteDirs(prefix string) []string {
+	dir, base := filepath.Split(prefix)
+	openDir := dir
+	if dir == "" {
+		openDir = "."
+	}
+	dents, err := os.ReadDir(openDir)
+	if err != nil {
+		return nil
+	}
+	var matches []string
+	for _, e := range dents {
+		name := e.Name()
+		if name == "" || !HasPrefix(name, base) {
+			continue
+		}
+		mode := e.Type()
+		path := filepath.Join(dir, name)
+		if mode&os.ModeSymlink != 0 {
+			fi, err := os.Stat(path)
+			if err != nil {
+				continue
+			}
+			mode = fi.Mode()
+		}
+		if mode.IsDir() {
+			matches = append(matches, path+"/")
+		}
+	}
+	return matches
+}
+
+func CompleteFiles(prefix string) []string {
+	dir, base := filepath.Split(prefix)
+	openDir := dir
+	if dir == "" {
+		openDir = "."
+	}
+	dents, err := os.ReadDir(openDir)
+	if err != nil {
+		return nil
+	}
+	if base != "" {
+		a := dents[:0]
+		for _, e := range dents {
+			if name := e.Name(); name != "" && HasPrefix(name, base) {
+				a = append(a, e)
+			}
+		}
+		dents = a
+	}
+	if len(dents) == 0 {
+		return nil
+	}
+
+	var matches []string
+	buf := make([]byte, 512)
+	for _, e := range dents {
+		mode := e.Type()
+		path := filepath.Join(dir, e.Name())
+		if mode&os.ModeSymlink != 0 {
+			fi, err := os.Stat(path)
+			if err != nil {
+				continue
+			}
+			mode = fi.Mode()
+		}
+
+		// if mode.IsDir() || (mode.IsRegular() && !isBinary(buf, path)) {
+		if mode.IsDir() {
+			matches = append(matches, path+"/")
+		}
+
+		// TODO: exclude images from the binary check
+		//
+		// Only check for binary files when there are fewer than
+		// BinaryFileCutoff matches or the file is executable.
+		if mode.IsRegular() &&
+			(len(dents) > BinaryFileCutoff && !isExecutable(path, mode)) ||
+			!isBinary(buf, path) {
+
+			matches = append(matches, path)
+		}
+	}
+	return matches
 }
 
 func main() {
 	subl := &complete.Command{
-		Args: complete.PredictFunc(Complete),
+		Args: complete.PredictFunc(CompleteFiles),
 		Flags: map[string]complete.Predictor{
-			"a ":                   predict.Nothing,
-			"add":                  predict.Nothing,
+			"a ":                   complete.PredictFunc(CompleteDirs),
+			"add":                  complete.PredictFunc(CompleteDirs),
 			"w ":                   predict.Nothing,
 			"wait":                 predict.Nothing,
 			"b ":                   predict.Nothing,
@@ -68,7 +194,7 @@ func main() {
 			"new-window":           predict.Nothing,
 			"command":              predict.Nothing,
 			"launch-or-new-window": predict.Nothing,
-			"project":              predict.Files("*.sublime-project"),
+			"project":              complete.PredictFunc(CompleteProject),
 		},
 	}
 	subl.Complete("subl")
