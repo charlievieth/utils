02ee569c71f5455c285808e1260aaf142f109b1b
Charlie Vieth <charlie.vieth@gmail.com>
2022-10-14 13:26:56 -0400

    gohashfiles: WIP add JSON encoder

--- gohashfiles/main.go
+++ gohashfiles/main.go
@@ -7,6 +7,7 @@ import (
 	"database/sql"
 	_ "embed"
 	"encoding/hex"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"hash"
@@ -38,6 +39,7 @@ func init() {
 type FileHash struct {
 	Name, Hash string
 	Size       int64
+	RunID      int64
 }
 
 type FileList struct {
@@ -64,6 +66,7 @@ type Worker struct {
 	ctx   context.Context
 	runID int64
 	stmt  *sql.Stmt
+	json  *jsonFileEncoder
 }
 
 func (w *Worker) doWork(wg *sync.WaitGroup, ch <-chan string) {
@@ -85,6 +88,34 @@ func (w *Worker) doWork(wg *sync.WaitGroup, ch <-chan string) {
 	}
 }
 
+// type BatchInerster struct {
+// 	mu    sync.Mutex
+// 	db    *sql.DB
+// 	stmt  *sql.Stmt
+// 	tx    *sql.Tx
+// 	count int64
+// }
+
+// func (b *BatchInerster) Insert(f *FileHash) error {
+// 	ext := strings.ToLower(filepath.Ext(f.Name))
+// 	base := filepath.Base(f.Name)
+// 	var pext *string
+// 	if ext != "" && ext != base {
+// 		pext = &ext
+// 	}
+// 	b.mu.Lock()
+// 	defer b.mu.Unlock()
+// 	_, err := b.stmt.Exec(f.RunID, f.Name, base, pext, f.Hash, f.Size)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	b.count++
+// 	if b.count == 128 {
+//
+// 	}
+// 	return err
+// }
+
 func (w *Worker) InsertFile(f *FileHash) error {
 	ext := strings.ToLower(filepath.Ext(f.Name))
 	base := filepath.Base(f.Name)
@@ -92,7 +123,7 @@ func (w *Worker) InsertFile(f *FileHash) error {
 	if ext != "" && ext != base {
 		pext = &ext
 	}
-	_, err := w.stmt.ExecContext(w.ctx, w.runID, f.Name, base, pext, f.Hash, f.Size)
+	_, err := w.stmt.Exec(f.RunID, f.Name, base, pext, f.Hash, f.Size)
 	return err
 }
 
@@ -120,19 +151,27 @@ func (w *Worker) HashFile(name string) error {
 	}
 	hashd := time.Since(start)
 	file := FileHash{
-		Name: name,
-		Hash: hex.EncodeToString(w.h.Sum(nil)),
-		Size: fi.Size(),
+		Name:  name,
+		Hash:  hex.EncodeToString(w.h.Sum(nil)),
+		Size:  fi.Size(),
+		RunID: w.runID,
 	}
 	// TODO: batch these
-	var dbErr error
+	var err1 error
+	var err2 error
 	if w.stmt != nil {
-		dbErr = w.InsertFile(&file)
+		err1 = w.InsertFile(&file)
+	}
+	if w.json != nil {
+		err2 = w.json.Encode(&file)
 	}
 	w.files.Add(file)
 	totald := time.Since(start)
 	debug.Printf("%s: hash: %s total: %s\n", name, hashd, totald)
-	return dbErr
+	if err1 != nil {
+		return err1
+	}
+	return err2
 }
 
 //go:embed sql/create_files_table.sql
@@ -187,6 +226,51 @@ func ignoreError(err error) bool {
 	return err == nil || os.IsPermission(err) || os.IsNotExist(err)
 }
 
+type jsonFileEncoder struct {
+	mu     sync.Mutex
+	file   *os.File
+	closed bool
+}
+
+func newJSONFileEncoder(name string) (*jsonFileEncoder, error) {
+	f, err := os.OpenFile(name, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &jsonFileEncoder{file: f}, nil
+}
+
+var newLine = []byte{'\n'}
+
+func (e *jsonFileEncoder) Encode(v any) error {
+	data, err := json.Marshal(v)
+	if err != nil {
+		return err
+	}
+	e.mu.Lock()
+	defer e.mu.Unlock()
+	if _, err = e.file.Write(data); err == nil {
+		_, err = e.file.Write(newLine)
+	}
+	return err
+}
+
+func (e *jsonFileEncoder) Close() error {
+	if e == nil {
+		return os.ErrInvalid
+	}
+	e.mu.Lock()
+	defer e.mu.Unlock()
+	if e.file == nil {
+		return os.ErrInvalid
+	}
+	if e.closed {
+		return os.ErrClosed
+	}
+	e.closed = true
+	return e.file.Close()
+}
+
 func realMain() error {
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s: PATHs...\n",
@@ -197,7 +281,10 @@ func realMain() error {
 	hashDB := flag.Bool("db", false, "write hashes to a sqlite database")
 	verbose := flag.Bool("verbose", false, "print verbose output")
 	hashDBName := flag.String("db-name", "", "name of hash database (implies -db)")
+	jsonFile := flag.String("json", "", "name of file to write JSON to")
 	numWorkers := flag.Int("workers", defaultNumWorkers(), "number of parallel workers to use")
+
+	_ = jsonFile
 
 	var globExclude GlobSet
 	flag.Var(&globExclude, "exclude", "exclude files GLOB")
@@ -216,6 +303,15 @@ func realMain() error {
 	}
 	if *verbose {
 		debug.SetOutput(os.Stderr)
+	}
+
+	var encoder *jsonFileEncoder
+	if *jsonFile != "" {
+		var err error
+		encoder, err = newJSONFileEncoder(*jsonFile)
+		if err != nil {
+			return err
+		}
 	}
 
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGQUIT)
@@ -280,6 +376,7 @@ func realMain() error {
 			runID:   runID,
 			stmt:    stmt,
 			ctx:     ctx,
+			json:    encoder,
 		}
 		go w.doWork(wg, workCh)
 	}
@@ -400,3 +497,107 @@ func main() {
 		os.Exit(1)
 	}
 }
+
+/*
+var bytesRead uint64
+
+func copyFile(dst io.Writer, src *os.File, buf []byte) (written int64, err error) {
+	const interval = time.Second / 10
+	if buf == nil {
+		buf = make([]byte, 32*1024)
+	}
+
+	timer := globalTimerPool.Get(interval)
+	defer globalTimerPool.Put(timer)
+	tick := make(chan struct{})
+	defer close(tick)
+
+	go func() {
+		for {
+			select {
+			case _, ok := <-tick:
+				if !ok {
+					return
+				}
+				if !timer.Stop() {
+					<-timer.C
+				}
+				timer.Reset(interval)
+			case <-timer.C:
+
+			}
+		}
+	}()
+
+	for {
+		nr, er := src.Read(buf)
+		if nr > 0 {
+			select {
+			case tick <- struct{}{}:
+			default:
+			}
+			nw, ew := dst.Write(buf[0:nr])
+			if nw < 0 || nr < nw {
+				nw = 0
+				if ew == nil {
+					ew = errors.New("invalid write result")
+				}
+			}
+			written += int64(nw)
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er != nil {
+			if er != io.EOF {
+				err = er
+			}
+			break
+		}
+	}
+
+	return 0, nil
+}
+
+// global pool of *time.Timer's. can be used by multiple goroutines concurrently.
+var globalTimerPool timerPool
+
+// timerPool provides GC-able pooling of *time.Timer's.
+// can be used by multiple goroutines concurrently.
+type timerPool struct {
+	p sync.Pool
+}
+
+// Get returns a timer that completes after the given duration.
+func (tp *timerPool) Get(d time.Duration) *time.Timer {
+	if t, _ := tp.p.Get().(*time.Timer); t != nil {
+		t.Reset(d)
+		return t
+	}
+
+	return time.NewTimer(d)
+}
+
+// Put pools the given timer.
+//
+// There is no need to call t.Stop() before calling Put.
+//
+// Put will try to stop the timer before pooling. If the
+// given timer already expired, Put will read the unreceived
+// value if there is one.
+func (tp *timerPool) Put(t *time.Timer) {
+	if !t.Stop() {
+		select {
+		case <-t.C:
+		default:
+		}
+	}
+
+	tp.p.Put(t)
+}
+*/
