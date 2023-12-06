474aa7051e895397489fd4bfaa33b2b65a3f13d1
Charlie Vieth <charlie.vieth@mongodb.com>
2023-12-05 22:20:40 -0500

    sortimports/gosortimports: replace sortimports with gosortimports

---
+++ gosortimports/doc.go
@@ -0,0 +1,11 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Command gosortimports is a more aggressive version
+of goimports that handles very badly grouped imports.
+
+	$ go install github.com/charlievieth/utils/gosortimports
+*/
+package main
---
+++ gosortimports/go.mod
@@ -0,0 +1,7 @@
+module github.com/charlievieth/utils/gosortimports
+
+go 1.19
+
+require golang.org/x/tools v0.16.0
+
+require golang.org/x/mod v0.14.0 // indirect
